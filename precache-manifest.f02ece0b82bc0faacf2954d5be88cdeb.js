7e9e1ffe82be207b183bbe304552f83dff751ad4
kasettakorn <kasettakorn_techno@hotmail.com>
2020-04-07 18:20:50 +0700

    Updates

---
+++ precache-manifest.f02ece0b82bc0faacf2954d5be88cdeb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e08be7137eac32ae234987049563e56",
+    "url": "/curfewCountdown/index.html"
+  },
+  {
+    "revision": "b46e6a32565cee6133ac",
+    "url": "/curfewCountdown/static/css/main.4bd8b62a.chunk.css"
+  },
+  {
+    "revision": "1863f57b834c06fc72a3",
+    "url": "/curfewCountdown/static/js/2.03e68400.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/curfewCountdown/static/js/2.03e68400.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b46e6a32565cee6133ac",
+    "url": "/curfewCountdown/static/js/main.f887b7a0.chunk.js"
+  },
+  {
+    "revision": "2a4fe7a99998c1781ecf",
+    "url": "/curfewCountdown/static/js/runtime-main.db8ef695.js"
+  },
+  {
+    "revision": "702c57c0eb32bcf0e03dece2eeef4ca4",
+    "url": "/curfewCountdown/static/media/easter-4978337_1920.702c57c0.jpg"
+  }
+]);
